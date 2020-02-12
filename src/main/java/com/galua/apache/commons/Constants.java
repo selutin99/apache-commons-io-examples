3ec2ef2ef090166c3f0f6f4a81c8ca42361b9698
selutin99 <cool.selutin99@yandex.ru>
2020-02-12 10:22:49 +0400

    init commit and first example

---
+++ src/main/java/com/galua/apache/commons/Constants.java
@@ -0,0 +1,5 @@
+package com.galua.apache.commons;
+
+public interface Constants {
+    String RESOURCE_FOLDER = "src/main/resources/";
+}
---
+++ src/main/java/com/galua/apache/commons/IOUtilsExample.java
@@ -0,0 +1,40 @@
+package com.galua.apache.commons;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class IOUtilsExample {
+
+    private static String FILE_NAME = Constants.RESOURCE_FOLDER + "IOUtils.txt";
+
+    public static void main(String[] args) {
+        try {
+            //Using BufferedReader
+            System.out.println("BufferedReader:");
+            readUsingTraditionalWay();
+
+            //Using IOUtils
+            System.out.println("\nApache Commons IO:");
+            readUsingIOUtils();
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void readUsingTraditionalWay() throws IOException {
+        try(BufferedReader bufferReader = new BufferedReader(new InputStreamReader(
+                new FileInputStream(FILE_NAME)))) {
+            String line;
+            while( ( line = bufferReader.readLine() ) != null ) System.out.println( line );
+        }
+    }
+
+    private static void readUsingIOUtils() throws IOException {
+        try(InputStream in = new FileInputStream(FILE_NAME)) {
+            System.out.println( IOUtils.toString( in , StandardCharsets.UTF_8) );
+        }
+    }
+}
